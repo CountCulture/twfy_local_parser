d5d9e95a5cb3c8faab5e40dc053140fecf5f9a3e
CountCulture <countculture@googlemail.com>
2009-04-22 17:24:45 +0100

    Added committees index action and bit more info on views

--- app/controllers/committees_controller.rb
+++ app/controllers/committees_controller.rb
@@ -1,6 +1,13 @@
 class CommitteesController < ApplicationController
+  
+  def index
+    @council = Council.find(params[:council_id])
+    @committees = @council.committees
+  end
+  
   def show
     @committee = Committee.find(params[:id])
+    @council = @committee.council
   end
-
+  
 end
--- test/functional/committees_controller_test.rb
+++ test/functional/committees_controller_test.rb
@@ -14,4 +14,36 @@ class CommitteesControllerTest < ActionController::TestCase
 
    end  
 
+   # index test
+   context "on GET to :index with council_id" do
+     setup do
+       @committee = Factory(:committee)
+       @council = @committee.council
+       Factory.create(:committee, :council_id => @council.id, :uid =>@committee.uid+1, :title => "another committee", :url => "http://foo.com" )
+       Factory.create(:committee, :council_id => Factory(:another_council).id, :uid => @committee.uid+1, :title => "another council's committee", :url => "http://foo.com" )
+       get :index, :council_id => @council.id
+     end
+
+     should_assign_to :committees
+     should_assign_to(:council) { @council }
+     should_respond_with :success
+     should_render_template :index
+     should "should list committees for council" do
+       assert_select "ul li", 2 do
+        
+       end
+     end
+   end
+   
+   context "on GET to :index without council_id" do
+     setup do
+       @committee = Factory(:committee)
+     end
+
+     # should_respond_with :failure
+     should "raise an exception" do
+       assert_raise(ActiveRecord::RecordNotFound) { get :index }
+     end
+   end
+   
 end
