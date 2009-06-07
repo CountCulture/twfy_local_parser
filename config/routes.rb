49aa4a7d3a04bdfc2bbe7c5e1dc84d809492c8ed
CountCulture <countculture@googlemail.com>
2009-06-07 19:41:20 +0100

    Added basic formatting and dummy front page

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "main"
 
   # See how all your routes lay out with "rake routes"
 
