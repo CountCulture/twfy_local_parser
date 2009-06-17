c62296d60b5cae7be0a54c33acfb856e4abf54d7
CountCulture <countculture@googlemail.com>
2009-06-17 11:27:49 +0100

    Bugfix: updated_at was ambiguous

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    @councils = Council.parsed.find(:all, :order => "updated_at DESC", :limit => 10)
+    @councils = Council.parsed.find(:all, :order => "councils.updated_at DESC", :limit => 10)
   end
 
 end
