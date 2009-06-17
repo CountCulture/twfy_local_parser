c5c983d28dde57f7c9d0835825678385ba515866
CountCulture <countculture@googlemail.com>
2009-06-17 12:05:02 +0100

    Added MeetingsController#index to show all meeting for given council

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,7 +1,19 @@
 class MeetingsController < ApplicationController
   
+  def index
+    @council = Council.find(params[:council_id])
+    @title = "Committee meetings for #{@council.title}"
+    @meetings = @council.meetings.find(:all, :order => "date_held DESC")
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @meetings.to_xml }
+      format.json { render :xml => @meetings.to_json }
+    end
+  end
+  
   def show
     @meeting = Meeting.find(params[:id])
+    @council = @meeting.council
     @committee = @meeting.committee
     @other_meetings = @committee.meetings - [@meeting]
     @title = @meeting.title
--- test/functional/meetings_controller_test.rb
+++ test/functional/meetings_controller_test.rb
@@ -1,16 +1,67 @@
 require 'test_helper'
 
 class MeetingsControllerTest < ActionController::TestCase
+  
+  def setup
+    @committee = Factory(:committee)
+    @council = @committee.council
+    @member = Factory(:member, :council => @council)
+    @meeting = Factory(:meeting, :council => @council, :committee => @committee)
+    @another_meeting = Factory(:meeting, :date_held => 3.days.from_now.to_date, :council => @council, :committee => @committee, :uid => @meeting.uid+1)
+    @committee.members << @member
+  end
+  
+  # index tests
+  context "on GET to :index for council" do
+    
+    context "with basic request" do
+      setup do
+        get :index, :council_id => @council.id
+      end
+  
+      should_assign_to(:council) { @council } 
+      should_assign_to(:meetings) { [@another_meeting, @meeting] } # most recent first
+      should_respond_with :success
+      should_render_template :index
+      should_respond_with_content_type 'text/html'
+      
+      should "list meetings" do
+        assert_select "#meetings ul a", @meeting.title
+      end
+      
+      should "have title" do
+        assert_select "title", /Committee meetings for #{@council.title}/
+      end
+    end
+        
+    context "with xml requested" do
+      setup do
+        get :index, :council_id => @council.id, :format => "xml"
+      end
+  
+      should_assign_to(:council) { @council } 
+      should_assign_to(:meetings) { [@another_meeting, @meeting]} # most recent first
+      should_respond_with :success
+      should_render_without_layout
+      should_respond_with_content_type 'application/xml'
+    end
+    
+    context "with json requested" do
+      setup do
+        get :index, :council_id => @council.id, :format => "json"
+      end
+  
+      should_assign_to(:council) { @council } 
+      should_assign_to(:meetings) { [@another_meeting, @meeting]} # most recent first
+      should_respond_with :success
+      should_render_without_layout
+      should_respond_with_content_type 'application/json'
+    end
+  end
+
+  # show tests
   context "on GET to :show" do
     
-    setup do
-      @committee = Factory(:committee)
-      @member = Factory(:member, :council => @committee.council)
-      @meeting = Factory(:meeting, :council => @committee.council, :committee => @committee)
-      @another_meeting = Factory(:meeting, :date_held => 3.days.from_now.to_date, :council => @committee.council, :committee => @committee, :uid => @meeting.uid+1)
-      @committee.members << @member
-    end
-
     context "with basic request" do
       setup do
         get :show, :id => @meeting.id
